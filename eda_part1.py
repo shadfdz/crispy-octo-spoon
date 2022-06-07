73492685b163e92706e5533fab33c59dafb66b8d
Shad Fernandez <shadfernandez@Shads-MacBook-Pro.local>
2022-06-07 08:59:04 -0700

    completed eda_part1

--- eda_part1.py
+++ eda_part1.py
@@ -1,9 +1,10 @@
-import sys
+from locale import normalize
 
 import numpy as np
 import pandas as pd
 from plotly import express as px
 from plotly import figure_factory as ff
+from plotly import graph_objects as go
 
 
 def plot_cat_resp_cont_predictors(df, predictor, response, f_name, bin_size=1):
@@ -34,51 +35,103 @@ def plot_cat_resp_cont_predictors(df, predictor, response, f_name, bin_size=1):
     # fig.show()
 
 
-def main():
+# loading our data
+df = pd.read_csv("./dataset/online_shoppers_intention.csv")
 
-    # loading our data
-    df = pd.read_csv("./dataset/online_shoppers_intention.csv")
+# shape
+print(df.shape)
 
-    # # shape
-    print(df.shape)
+# # looking at our data
+print(df.dtypes)
 
-    # # looking at our data
-    print(df.dtypes)
+# look at descriptive stats
+print(df.describe().T)
+print(df.describe(include=["object", "bool"]).T)
 
-    # look at descriptive stats
-    print(df.describe().T)
-    print(df.describe(include=["object", "bool"]).T)
+# spit our variables according to type
+categorical_var = [
+    "SpecialDay",
+    "Month",
+    "OperatingSystems",
+    "Browser",
+    "Region",
+    "TrafficType",
+    "VisitorType",
+    "Revenue",
+    "Weekend",
+]
+continuous_var = df.loc[:, ~df.columns.isin(categorical_var)].columns
 
-    # spit our variables according to type
-    categorical_var = [
-        "SpecialDay",
-        "Month",
-        "OperatingSystems",
-        "Browser",
-        "Region",
-        "TrafficType",
-        "VisitorType",
-        "Revenue",
-        "Weekend",
+# view skewness of data
+print(df[continuous_var].skew())
+
+# plot info duration
+plot_cat_resp_cont_predictors(df, continuous_var[3], "Revenue", continuous_var[3], 30)
+
+# log transform and plot info duration
+df["info_duration_log"] = np.log(df[continuous_var[3]] + 1)
+plot_cat_resp_cont_predictors(
+    df, "info_duration_log", "Revenue", "info_duration_log", 0.25
+)
+
+# plot months
+months = df.Month.value_counts().index
+fig = go.Figure(
+    data=[
+        go.Bar(
+            name="False",
+            x=months,
+            y=df[df["Revenue"] == False].Month.value_counts(normalize=True).to_list(),
+        ),
+        go.Bar(
+            name="True",
+            x=months,
+            y=df[df["Revenue"] == True].Month.value_counts(normalize=True).to_list(),
+        ),
     ]
+)
 
-    continuous_var = df.loc[:, ~df.columns.isin(categorical_var)].columns
+# Change the bar mode
+fig.update_layout(
+    barmode="stack",
+    title="Revenue by Month",
+    xaxis_title="Month",
+    yaxis_title="Distribution",
+)
 
-    print(df[continuous_var].skew())
+fig.write_html(
+    file="./output/" + "Revenue_by_month" + ".html",
+    include_plotlyjs="cdn",
+)
+# fig.show()
 
-    plot_cat_resp_cont_predictors(
-        df, continuous_var[3], "Revenue", continuous_var[3], 30
-    )
+# plot region
+Region = df.Region.value_counts().index
+fig = go.Figure(
+    data=[
+        go.Bar(
+            name="False",
+            x=Region,
+            y=df[df["Revenue"] == False].Region.value_counts(normalize=True).to_list(),
+        ),
+        go.Bar(
+            name="True",
+            x=Region,
+            y=df[df["Revenue"] == True].Region.value_counts(normalize=True).to_list(),
+        ),
+    ]
+)
 
-    df["info_duration_log"] = np.log(df[continuous_var[3]] + 1)
+# Change the bar mode
+fig.update_layout(
+    barmode="stack",
+    title="Revenue by Region",
+    xaxis_title="Region",
+    yaxis_title="Count",
+)
 
-    plot_cat_resp_cont_predictors(
-        df, "info_duration_log", "Revenue", "info_duration_log", 0.25
-    )
-
-    fig = px.histogram(df["Revenue"], x="Month")
-    fig.show()
-
-
-if __name__ == "__main__":
-    sys.exit(main())
+fig.write_html(
+    file="./output/" + "Revenue_by_Region" + ".html",
+    include_plotlyjs="cdn",
+)
+fig.show()
